d51e93be1da89053b1257f0ee277e55c5dcf5dc5
asmitadarokar <adarokar671996@gmail.com>
2026-03-12 21:37:45 +0530

    change in route

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -7,6 +7,6 @@ use Illuminate\Http\Request;
 class AdminController extends Controller
 {
     public function Admindashboard(){
-        return redirect()->route('category.list');
+        return redirect()->route('material.list');
     }
 }
